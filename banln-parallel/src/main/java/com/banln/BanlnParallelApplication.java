45290111267186df09a58e7499a8f0d840fe1b44
banlnsilva <yukikaze92@naver.com>
2022-04-26 11:38:32 +0900

    commit1 등록

---
+++ banln-parallel/src/main/java/com/banln/BanlnParallelApplication.java
@@ -0,0 +1,13 @@
+package com.banln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanlnParallelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanlnParallelApplication.class, args);
+	}
+
+}
---
+++ banln-parallel/src/test/java/com/banln/BanlnParallelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanlnParallelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
